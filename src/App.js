e94d67dc74d20160241ccfd2ef4c9fb028bfdf4f
Achintya <achintyashende@gmail.com>
2024-11-25 02:02:25 +0530

    Add all project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';  // Import the Dashboard component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task Dashboard</h1>
+      <Dashboard />  {/* Render the Dashboard component */}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
+import { store, persistor } from './redux/store';
+import Dashboard from './components/Dashboard';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      {/* <h1>Todo</h1> */}
+      <Dashboard />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
